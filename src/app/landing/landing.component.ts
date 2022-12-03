0269c499e089e877c3eeae0288a70aaff1bca270
”fabigo737” <fabiandegouveia@gmail.com>
2022-12-03 16:56:59 -0500

    Login and signup working as single page

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -9,6 +9,12 @@ import { Router } from '@angular/router';
 export class LandingComponent {
   hola = '<h1>Hola<h1>'
   constructor(private router: Router) {}
+  registrarse(){
+    this.router.navigate(['/users/signup'])
+  }
+  iniciarSesion(){
+    this.router.navigate(['/users/login'])
+  }
   ngOnInit(): void {
     if(sessionStorage.getItem('auth-token')){
       this.router.navigate(['/home'])
