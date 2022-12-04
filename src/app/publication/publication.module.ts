f8253af474869e95c187293beb34683930424137
”fabigo737” <fabiandegouveia@gmail.com>
2022-12-04 13:48:28 -0500

    test

---
+++ src/app/publication/publication.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PublicationRoutingModule } from './publication-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PublicationRoutingModule
+  ]
+})
+export class PublicationModule { }
