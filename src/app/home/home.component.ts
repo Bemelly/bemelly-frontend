423135d570acb6e6ac9bc5ad30b6d1296c5f8bbe
JuanPabloHincapieF <jphf0527@gmail.com>
2022-12-06 19:07:43 -0500

    home bug fixes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,60 +5,59 @@ import { TokenStorageService } from 'src/app/_services/token-storage.service';
 import { Router } from '@angular/router';
 import { PublicationService } from '../_services/publication.service';
 
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-user: any
- usuarios:any;
- profile: any
- defaultAvatar: string = 'https://t3.ftcdn.net/jpg/03/53/11/00/360_F_353110097_nbpmfn9iHlxef4EDIhXB1tdTD0lcWhG9.jpg'
-  constructor(private bookingService: BookingService, private profileService: ProfileService, private tokenService: TokenStorageService, private router: Router, private publicationSev: PublicationService) { }
+  user: any;
+  usuarios: any;
+  profile: any;
+  defaultAvatar: string =
+    'https://t3.ftcdn.net/jpg/03/53/11/00/360_F_353110097_nbpmfn9iHlxef4EDIhXB1tdTD0lcWhG9.jpg';
+  constructor(
+    private bookingService: BookingService,
+    private profileService: ProfileService,
+    private tokenService: TokenStorageService,
+    private router: Router,
+    private publicationSev: PublicationService
+  ) {}
 
   ngOnInit(): void {
-    this.user = this.tokenService.getUser()
-    this.getProfile()
+    this.user = this.tokenService.getUser();
+    this.getProfile();
     this.bookingService.getUsers().subscribe({
-      next: (data:any)=>{
+      next: (data: any) => {
         console.log(data);
-        this.usuarios = data
+        this.usuarios = data;
       },
-      error: (err:any)=>{
+      error: (err: any) => {
         console.log(err);
-        
-      }
-    })
+      },
+    });
   }
-  showPublication(data:any){
+  showPublication(data: any) {
     this.publicationSev.getPublication(data).subscribe({
-      next:(response: any)=>{
+      next: (response: any) => {
         console.log(response);
-        
-        this.router.navigate([`/users/showpulication/${data}`])
-        console.log(data);
-        
-      },
-      error:(error:any)=>{
-        console.log(error)
-        
-      }
-    })
 
-    
-  }
-  getProfile(){
-    this.profileService.getProfile().subscribe({
-      next:(response: any)=>{
-        this.profile = response.data
-        
+        this.router.navigate([`users/showpublication/${data}`]);
+        console.log(data);
       },
-      error:(error)=>{
-        console.log(error)
-        
-      }
-    })
-   }
+      error: (error: any) => {
+        console.log(error);
+      },
+    });
+  }
+  getProfile() {
+    this.profileService.getProfile().subscribe({
+      next: (response: any) => {
+        this.profile = response.data;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
 }
