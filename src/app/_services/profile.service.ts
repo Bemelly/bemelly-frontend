4ab05ba07fa21f8b0c13723bdab075462aeb9b8d
JuanPabloHincapieF <jphf0527@gmail.com>
2022-12-12 21:41:09 -0500

    fixing edit profile

--- src/app/_services/profile.service.ts
+++ src/app/_services/profile.service.ts
@@ -3,22 +3,30 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { TokenStorageService } from './token-storage.service';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProfileService {
   headers = new HttpHeaders({
     token: this.tokenService.getToken!,
-  })
+  });
   constructor(
     private http: HttpClient,
-    private tokenService: TokenStorageService) { }
-  uploadPhotoProfile(photoProfile: any){
-    return this.http.post(`${environment.apiUrl}/api/profile/updatePhotoProfile`, photoProfile, {headers: this.headers}
-    )
+    private tokenService: TokenStorageService
+  ) {}
+  uploadPhotoProfile(photoProfile: any) {
+    return this.http.post(
+      `${environment.apiUrl}/api/profile/updatePhotoProfile`,
+      photoProfile,
+      { headers: this.headers }
+    );
   }
-  getProfile(){
-    return this.http.get(`${environment.apiUrl}/api/profile/getProfile`, {headers: this.headers})
+  getProfile() {
+    return this.http.get(`${environment.apiUrl}/api/profile/getProfile`, {
+      headers: this.headers,
+    });
+  }
+  getUser(id: any): any {
+    return this.http.get(`${environment.apiUrl}/api/users/getUserById/${id}`);
   }
 }
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,16 +1,36 @@
-import { Component } from '@angular/core';
-import { BookingService } from '../_services/booking.service';
+import { Component, OnInit } from '@angular/core';
+
 import { ProfileService } from '../_services/profile.service';
 
+import { TokenStorageService } from '../_services/token-storage.service';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
-export class ProfileComponent {
-  constructor(private profileService: ProfileService, private bookingService: BookingService){}
+export class ProfileComponent implements OnInit {
+  user: any;
+  constructor(
+    private profileService: ProfileService,
+    private tokenService: TokenStorageService
+  ) {}
 
-  
-  
+  ngOnInit(): void {
+    this.userData();
+  }
+
+  userData() {
+    const tokenInfo: any = this.tokenService.getUser();
+
+    this.profileService.getUser(tokenInfo.CC).subscribe({
+      next: (data: any) => {
+        console.log(data);
+
+        this.user = data;
+      },
+      error: (err: any) => {
+        console.log(err);
+      },
+    });
+  }
 }
-
